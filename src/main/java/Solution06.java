1f696c189d433f94a7997af82b4ac9f55e061f3d
spesh <fernandezjonah98@gmail.com>
2021-09-16 18:46:14 -0400

    updates

--- src/main/java/Solution06.java
+++ src/main/java/Solution06.java
@@ -1,2 +1,30 @@
-public class Solution06 {
+/*
+ *
+ */
+
+
+import java.util.Scanner;
+
+public  class Solution06 {
+    // declare static scanner object
+
+    public static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int currentAge = getCurrentAge();
+        int retiredAge = getRetiredAge();
+
+    }
+
+    private static int getRetiredAge() {
+        System.out.println("What is your current age? ");
+        return in.nextInt();
+    }
+
+    private static int getCurrentAge() {
+        System.out.println("What age do you wish to retire? ");
+        return in.nextInt();
+    }
+
 }
